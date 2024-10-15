e2a0e3b93b792623b0d437889a38c15fc18f595c
Nguyen Quang Do <quangdo0206@gmail.com>
2024-10-15 22:27:33 +0700

    step 3

--- FrontEnd/src/pages/Home/index.js
+++ FrontEnd/src/pages/Home/index.js
@@ -1,18 +1,20 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import CarouselCustomize from '../../components/Carousel';
+import styles from './Home.module.scss';
+import RunningBanner from '../../components/RunningBanner';
+
+const cx = classNames.bind(styles);
+
 function Home() {
     return (
-        <h1>
-            <ul>
-                <li>hehe</li>
-                <li>hehe</li>
-                <li>hehe</li>
-                <li>hehe</li>
-                <li>hehe</li>
-                <li>hehe</li>
-                <li>hehe</li>
-                <li>hehe</li>
-                <li>hehe</li>
-            </ul>
-        </h1>
+        <div className={cx('inner')}>
+      
+            <div className={cx('carousel')}><CarouselCustomize /></div>
+           <RunningBanner />
+       
+        </div>
+       
     );
 }
 
